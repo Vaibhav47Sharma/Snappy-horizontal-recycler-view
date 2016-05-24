e5d6469dff61dacb549305c44a4c58ee2e03c4b3
vaibhav47sharma <vaibhav47sharma@gmail.com>
2016-05-24 16:51:59 +0530

    new horizontal space decorations

---
+++ app/src/main/java/practice/com/snappyhorizontal/HorizontalRecyclerDecoration.java
@@ -0,0 +1,29 @@
+package practice.com.snappyhorizontal;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by vaibhavsharma on 24/05/16.
+ * Give a horizontal spacing for elements.
+ */
+public class HorizontalRecyclerDecoration extends RecyclerView.ItemDecoration {
+  private int horizontalSpace;
+  private Context context;
+
+  public HorizontalRecyclerDecoration(Context context) {
+    this.context = context;
+    horizontalSpace = (int) context.getResources().getDimension(R.dimen.horizontal_offset);
+  }
+
+  @Override
+  public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+    //To prevent last item to get horizontal space.
+    if (parent.getChildAdapterPosition(view) != (parent.getAdapter().getItemCount() - 1)) {
+      outRect.right = horizontalSpace;
+    }
+    super.getItemOffsets(outRect, view, parent, state);
+  }
+}
