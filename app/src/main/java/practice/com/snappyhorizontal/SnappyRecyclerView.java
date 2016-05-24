794c14993c1154dece49e5e6317fe50fa6a501cd
Damanpreet Singh <damanpreet.s@hotmail.com>
2016-05-24 16:51:43 +0530

    adding utilities

--- app/src/main/java/practice/com/snappyhorizontal/SnappyRecyclerView.java
+++ app/src/main/java/practice/com/snappyhorizontal/SnappyRecyclerView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by vaibhavsharma on 24/05/16.
@@ -19,5 +21,35 @@ public class SnappyRecyclerView extends RecyclerView {
 
   public SnappyRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
     super(context, attrs, defStyle);
+  }
+  @Override
+  public void onScrolled(int dx, int dy) {
+    Log.d("Daman", "dx =" + dx + " dy = " + dy );
+    super.onScrolled(dx, dy);
+  }
+
+  @Override
+  public int getMinFlingVelocity() {
+    int minFlingVelocity = super.getMinFlingVelocity();
+    Log.d("Daman", minFlingVelocity + "min" );
+    return minFlingVelocity;
+  }
+
+  @Override
+  public int getMaxFlingVelocity() {
+    int maxFlingVelocity = super.getMaxFlingVelocity();
+    Log.d("Daman", maxFlingVelocity + " max" );
+    return maxFlingVelocity;
+  }
+
+  @Override
+  public boolean fling(int velocityX, int velocityY) {
+    Log.d("Daman", "vx =" + velocityX + " vy = " + velocityY );
+    return super.fling(velocityX, velocityY);
+  }
+
+  @Override
+  public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+    return super.onNestedFling(target, velocityX, velocityY, consumed);
   }
 }
